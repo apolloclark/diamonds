c43ff6bdf51b1ca2e76fb4a0be00a97b6a3a0af6
Apollo Clark <apolloclark@gmail.com>
2016-10-25 12:38:05 -0400

    updated utils/install_jenkins-pipeline.sh, fixed issues with being run when there's an existing Jenkins installation

--- utils/install_jenkins-pipeline.sh
+++ utils/install_jenkins-pipeline.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-# clone vagrant-jenkins-pipeline, install it
-cd ~/
+# set the environment to be fully automated
+export DEBIAN_FRONTEND="noninteractive"
+
+# remove Jenkins, if it's running
+if ps aux | grep -q ".*[j]enkins.war"; then
+	echo "INFO: Jenkins found, removing."
+	service jenkins stop
+	apt-get remove -y jenkins
+	rm -rf /var/lib/jenkins
+else
+	echo "INFO: Jenkins not found."
+fi
+
+# clear out staging folders
+rm -rf ./vagrant-jenkins-pipeline
+rm -rf /vagrant/
+
+# clone vagrant-jenkins-pipeline, copy install files
 git clone https://github.com/apolloclark/vagrant-jenkins-pipeline
 cd ./vagrant-jenkins-pipeline
-sudo cp -R ./data /vagrant/
+cp -rf ./data /vagrant/
 cd ./provision
-sudo bash ./bootstrap.sh
-sudo bash ./bootstrap_python.sh
+
+# set the install scripts to executable, run them
+chmod +x ./*.sh
+./bootstrap.sh
+./bootstrap_python.sh
